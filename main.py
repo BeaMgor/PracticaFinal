f7d3e2b304fd9fb18f188c20be27f44a7f84f232
Beatriz M Gordillo <beatriz.mgordillo@gmail.com>
2019-02-10 19:02:08 +0100

    Botones y semana mes

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 from tkinter import *
 from tkinter import ttk
 from datetime import * 
+import calendar
 
+
+
+month_names = tuple(calendar.month_name)
 
 class Calendar(ttk.Frame):
-    
+     
+
     def __init__(self, parent, **args):
-        ttk.Frame.__init__(self, parent, height=args['height'],width=args['width'])  
+        ttk.Frame.__init__(self, parent, height=args['height'],width=args['width']) #para informar si o si de la altura y anchura 
+        self.pack_propagate(0)
 
-
-        self.header = Frame(self, width = 532, height = 40).place(x=0, y=0)
-        self.__btnLastYear = Button(self, text = '<<', font=('Arial',24), width = 3, height= 1).place(x=10, y=5)
-        self.__btnLastMonth = Button(self, text = '<', font=('Arial',24), width = 3, height= 1).place(x=100, y=5)
-        self.__btnNextMonth = Button(self, text = '>', font=('Arial',24), width = 3, height= 1).place(x=380,y=5)
-        self.__btnNextYear = Button(self, text = '>>', font=('Arial',24), width = 3, height= 1).place(x=470, y=5)
-        self.lblMonth = Label(self,text = 'Enero 2019', font = ('Arial', 28, 'bold')).place(x=180,y=3)
-
-
-        self.daysName = Frame(self, width = 71, height = 20)
         
-        self.__Lunes = Label(self, font=('Arial', 11), text = 'Lunes', width=9, height=2).place(x=0,y=38)
+        self.__btnLastYear = ttk.Button(self, width = 3,text = '<<', command = self._backYear).place(x=10, y=5)
+        self.__btnLastMonth = ttk.Button(self, text = '<', width = 3,command = self._backMonth).place(x=100, y=5)
+        self.__btnNextMonth = ttk.Button(self, text = '>', width = 3, command = self._nextMonth).place(x=380,y=5)
+        self.__btnNextYear = ttk.Button(self, text = '>>', width = 3, command = self._nextYear).place(x=470, y=5)
+        self.lblMonth = ttk.Label(self,text = 'Enero 2019', font = ('Arial', 28, 'bold')).place(x=180,y=3)
+
+        self.__Lunes = ttk.Label(self, width=9,font=('Arial', 11), text = 'Lunes').place(x=0,y=38)
        
-        self.__Martes = Label(self, font=('Arial', 11), text = 'Martes', width=9, height=2).place(x=64,y=38)
-        self.__Miércoles = Label(self, font=('Arial', 11), text = 'Miércoles', width=13, height=2).place(x=128,y=38)
-        self.__Jueves = Label(self, font=('Arial', 11), text = 'Jueves', width=12, height=2).place(x=222,y=38)
-        self.__Viernes = Label(self, font=('Arial', 11), text = 'Viernes', width=12, height=2).place(x=306,y=38)
-        self.__Sábado = Label(self, font=('Arial', 11), text = 'Sábado', width=11, height=2).place(x=390,y=38)
-        self.__Domingo = Label(self, font=('Arial', 11), text = 'Domingo', width=8, height=2).place(x=470,y=38)
+        self.__Martes = ttk.Label(self, font=('Arial', 11), text = 'Martes', width=9).place(x=64,y=38)
+        self.__Miércoles = ttk.Label(self, font=('Arial', 11), text = 'Miércoles', width=13).place(x=128,y=38)
+        self.__Jueves = ttk.Label(self, font=('Arial', 11), text = 'Jueves', width=12).place(x=222,y=38)
+        self.__Viernes = ttk.Label(self, font=('Arial', 11), text = 'Viernes', width=12).place(x=306,y=38)
+        self.__Sábado = ttk.Label(self, font=('Arial', 11), text = 'Sábado', width=11).place(x=390,y=38)
+        self.__Domingo = ttk.Label(self, font=('Arial', 11), text = 'Domingo', width=8).place(x=470,y=38)
         
+
+
+    def _backYear(self):
+        self.__valor+=1
+        self.__label.config(text=str(self.__valor))
     
+    def _nextYear(self):
+        self.__valor+=1
+        self.__label.config(text=str(self.__valor))
+    def _backMonth(self):
+        self.__valor+=1
+        self.__label.config(text=str(self.__valor))
+    def _nextMonth(self):
+        self.__valor+=1
+        self.__label.config(text=str(self.__valor))    
+
 
 
 
 class month(ttk.Frame):
     def __init__(self, parent, **args):
-        ttk.Frame.__init__(self, parent) 
+        ttk.Frame.__init__(self, parent,height=args['height'],width=args['width']) 
 
     #self.dates = 
 
@@ -52,15 +69,16 @@ class month(ttk.Frame):
 
 
 class MainApp (Tk):
-    __Calendar = None
+    
 
     def __init__(self):
         Tk.__init__(self)
         self.geometry('532x422')
         self.title('Calendario Universal')  
         
-        self.__Calendar = Calendar(self, width=532, height=422).place(x=0,y=0)
-    
+        self.__Calendar = Calendar(self, width=532, height=422)
+        self.__Calendar.place(x=0,y=0)
+        
     
     
     def start(self):
